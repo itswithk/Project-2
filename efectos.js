2dd7344b15825b7d3bbdc7fe078418c9f5e8f198
karlaceballos <karlaisabelcc@gmail.com>
2021-09-21 18:57:43 -0500

    primer commit

---
+++ efectos.js
@@ -0,0 +1,35 @@
+const nombreCancion1 = () =>{
+   
+   /* let nombre = "Lalala";*/
+    alert(Lalala);
+}
+
+const nombreCancion2 = () =>{
+
+    let nombre = "Pogo";
+    alert(nombre);
+}
+
+const nombreCancion3 = () =>{
+
+    let nombre = "Spaceships";
+    alert(nombre);
+}
+
+const nombreCancion4 = () =>{
+
+    let nombre = "Monalisa";
+    alert(nombre);
+}
+
+const nombreCancion5 = () =>{
+
+    let nombre = "Glad you came";
+    alert(nombre);
+}
+
+const nombreCancion6 = () =>{
+
+    let nombre = "Help";
+    alert(nombre);
+}
